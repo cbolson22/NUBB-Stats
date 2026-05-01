bc01f2767fffdc0f9f7bcdf111624cf2cabac984
Connor Olson <121678858+cbolson22@users.noreply.github.com>
2026-05-01 17:29:31 -0400

    Games page record and collapsed

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,5 +7,9 @@ class GamesController < ApplicationController
                        .where(seasons: { year: season_year })
                        .order(date: :asc)
                        .includes(player_game_stats: :player)
+    @wins        = @games.where(result: "W").count
+    @losses      = @games.where(result: "L").count
+    @conf_wins   = @games.where(result: "W", conference_game: true).count
+    @conf_losses = @games.where(result: "L", conference_game: true).count
   end
 end
